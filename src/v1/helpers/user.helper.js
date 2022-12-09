dd7a03c0901aedab099314d9106c141985f265b8
Nathan Gnankadja <gnankadjanathan@gmail.com>
2022-12-09 14:58:09 +0100

    Create a hleper to generate user reference

---
+++ src/v1/helpers/user.helper.js
@@ -0,0 +1,30 @@
+const { getLastUser } = require("../services/user.service");
+
+const generateUserReference = async () => {
+
+    try {
+        let lastUserReference;
+        const lastUser = await getLastUser();
+
+        if (lastUser != null) {
+            lastUserReference = lastUser.user_reference;
+        }
+        else lastUserReference = "USER" + '0';
+
+        let id = lastUserReference.replace(/\D/g, '');
+
+        if (id !== '') {
+            id = Number.parseInt(id); id++;
+            let charId = String(id);
+
+            if (charId.length < 6) {
+                charId = charId.padStart(6, '0');
+            }
+
+            let newReference = 'USER' + charId;
+            return newReference;
+        }
+        else throw new Error("User reference generation error");
+    }
+    catch (error) { throw new Error("An user reference generation error was observed; Please try again later") }
+}
