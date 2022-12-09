ccadf5663dbf3126767eec4ff5842526662bf7aa
Nathan Gnankadja <gnankadjanathan@gmail.com>
2022-12-09 10:18:51 +0100

    Update Readme

---
+++ index.js
@@ -0,0 +1,36 @@
+// Dependencies
+const express = require('express');
+const dotenv = require("dotenv");
+const v1 = require("./src/v1/index");
+const DatabaseService = require("./src/v1/services/database.service.js");  //Try connect to database to check if service is started succeful
+
+
+
+// Instantiation && Initiation
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, }));
+app.use(cors(corsOption));
+
+
+// Manage Versionning
+app.use("/v1", v1);
+
+
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+    const statusCode = err.status || 500;
+    const message = (err.message && err.message.fr) || "Une erreur interne s'est produit; Veuillez réssayer plus tard";
+    res.status(statusCode).json({ status: statusCode, message: message });
+    return;
+});
+
+
+// Running Server
+app.listen(port, '127.0.0.1', () => {
+    console.log(`Application listening at http://127.0.0.1:${port}`)
+});
